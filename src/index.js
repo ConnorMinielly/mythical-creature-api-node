3bba188d59c41d3ba14246eb8b5a75aa6a1aeb2e
ConnorMinielly <Connor_Minielly@Outlook.com>
2018-09-13 01:31:32 -0400

    Minor package changes and clean up

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import { GraphQLServer } from 'graphql-yoga';
-import { importSchema } from 'graphql-import';
 import resolvers from './server/resolvers';
 import typeDefs from './server/mythics.js';
 import db from './database/mongo';
 import Mythics from './database/mythics';
-
-// const typeDefs = importSchema('./server/mythics.graphql');
 
 const options = {
   port: 3003,
