087bfa301d0be6a2d827635e75c841bd056169fc
ConnorMinielly <Connor_Minielly@Outlook.com>
2018-09-13 00:44:49 -0400

    it works, I am a god.

---
+++ src/server/mythics.js
@@ -0,0 +1,28 @@
+export default `
+  type Query {
+    mythics: [Mythic!]!
+    mythic(name: String): Mythic!
+  }
+
+  type Mutation {
+    summon(
+      name: String!
+      description: String!
+      country: String!
+      powers: [String!]!
+      weaknesses: [String!]!
+      rank: Int!
+    ): Boolean!
+    banish(name: String!): Boolean!
+  }
+
+  type Mythic {
+    id: String!
+    name: String!
+    description: String!
+    country: String!
+    powers: [String!]!
+    weaknesses: [String!]!
+    rank: Int!
+  }
+`;
